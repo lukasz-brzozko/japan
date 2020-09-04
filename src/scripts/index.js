16abadb58a985a5d601bd3ec420abce08a70a24f
Lukasz Brzozko <lukasz9099@gmail.com>
2020-09-04 19:09:21 +0200

    Add parallax effect

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,11 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
+import Parallax from "parallax-js";
 
-if (process.env.NODE_ENV === 'development') {
-  require('../index.html');
+if (process.env.NODE_ENV === "development") {
+  require("../index.html");
 }
 
-console.log('webpack starterkit');
+const SCENE_ELEMENT_ID = "scene";
+
+const sceneElement = document.getElementById(SCENE_ELEMENT_ID);
+const parallaxInstance = new Parallax(sceneElement);
